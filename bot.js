98a0ae71ad923f6ed3f2613d4d6a00d1814d6df5
Ava Cordero <corderda@oregonstate.edu>
2019-12-17 19:32:53 -0800

    Write basic help menu.

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
+/*
+PirateBot
+	by Luna Catastrophe
+*/
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json')
-const pre = "ar!"
+
+var pre = "ar!"
+
+
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -9,16 +17,82 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (msg.content.startsWith(pre)) {
-		var cmd = msg.content.replace(pre,"");
-		console.log("Command sent: " + cmd);
-		switch (cmd) {
+		var cmd = msg.content.replace(pre,"").split(" ");
+		console.log("Command received: '" + cmd + "'");
+		switch (cmd[0]) {
 			case "help":
-				msg.reply("Help Menu: (etc)");
+				helpMenu(msg, cmd);
 				break;
-			default:
-				msg.reply("Sorry, '" + cmd + "' is not a command. Please type '" + pre + "help' to learn more.");
+			case "config":
+				setConfig();
+				break;
+			case "start":
+				startAdventure();
+				break;
+			case "stats":
+				getStats();
+				break;
+			case "explore":
+				exploreChannel();
+				break;
+			case "pillage":
+				pillageTreasure();
+				break;
+			case "booty":
+				viewBooty();
+				break;
+			case "plunder":
+				plunderUser();
+				break;					
+			// default:
+			// 	msg.reply("Sorry, '" + cmd + "' is not a command. Please type '" + pre + "help' to learn more.");
 		}
 	}
 });
+
+function helpMenu(msg, cmd) {
+
+	switch (cmd[1]) {
+		case "config":
+			helpMenuConfig();
+			break;
+		case "start":
+			msg.reply(`\`${pre}start\`: Starts your pirate adventure.`);
+			break;
+		case "stats":
+			msg.reply(`\`${pre}stats\`: Shows your level, stats, gold, and loot amount.`);
+			break;
+		case "explore":
+			msg.reply(`\`${pre}explore\`: Looks for treasure in the current or default channel.`);
+			break;
+		case "pillage":
+			msg.reply(`\`${pre}pillage\`: Snatches up treasure and adds it to your loot stash.`);
+			break;
+		case "booty":
+			msg.reply(`\`${pre}booty\`: Shows your loot stash.`);
+			break;
+		case "plunder":
+			msg.reply(`\`${pre}plunder <pirate>\`: Attempts to steal loot and gold from another pirate.`);
+			break;	
+		default:
+			msg.reply(`here is the PirateBot Help Menu:
+				\`${pre}help\`: Brings up this menu.
+
+				\`${pre}config\`: Set PirateBot's configuration (Admin).
+
+				\`${pre}start\`: Starts your pirate adventure.
+				
+				\`${pre}stats\`: Shows your level, stats, gold, and loot amount.
+				
+				\`${pre}explore\`: Looks for treasure in the current or default channel.
+				
+				\`${pre}pillage\`: Snatches up treasure and adds it to your loot stash.
+				
+				\`${pre}booty\`: Shows your loot stash.
+				
+				\`${pre}plunder <pirate>\`: Attempts to steal loot and gold from another pirate.`);
+	}
+
+}
 
 client.login(auth.token);
