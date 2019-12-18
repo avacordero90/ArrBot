3525ea2ce1b6ec4f9dd9f9e2c04051f04462eab4
Ava Cordero <corderda@oregonstate.edu>
2019-12-17 17:31:24 -0800

    Upgrade to discord.js.

--- bot.js
+++ bot.js
@@ -1,43 +1,15 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json')
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-logger.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `ar!`
-    var prefix = 'ar!'
-    if (message.startsWith(prefix)) {
-        console.log("true")
-        var args = message.split(prefix);
-        console.log(args)
-        var cmd = args[1];
-       
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+client.login(auth.token);
