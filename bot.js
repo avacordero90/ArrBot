027af742c651be68f7b237763037c4f277dcdbc3
Ava Cordero <corderda@oregonstate.edu>
2019-12-17 18:08:03 -0800

    basic command-reply system set up.

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json')
+const pre = "ar!"
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+	if (msg.content.startsWith(pre)) {
+		var cmd = msg.content.replace(pre,"");
+		console.log("Command sent: " + cmd);
+		switch (cmd) {
+			case "help":
+				msg.reply("Help Menu: (etc)");
+				break;
+			default:
+				msg.reply("Sorry, '" + cmd + "' is not a command. Please type '" + pre + "help' to learn more.");
+		}
+	}
 });
 
 client.login(auth.token);
