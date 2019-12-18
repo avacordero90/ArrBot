50ed5042e979ca0c02b5fd8c44e865477c9e744b
Ava Cordero <corderda@oregonstate.edu>
2019-12-17 22:29:49 -0800

    Implemented start, stats, and booty.

--- bot.js
+++ bot.js
@@ -7,50 +7,54 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json')
 
+var sevenSeas = {}
 var pre = "ar!"
 
 
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-	if (msg.content.startsWith(pre)) {
-		var cmd = msg.content.replace(pre,"").split(" ");
-		console.log("Command received: '" + cmd + "'");
-		switch (cmd[0]) {
-			case "help":
-				helpMenu(msg, cmd);
-				break;
-			case "config":
-				setConfig();
-				break;
-			case "start":
-				startAdventure();
-				break;
-			case "stats":
-				getStats();
-				break;
-			case "explore":
-				exploreChannel();
-				break;
-			case "pillage":
-				pillageTreasure();
-				break;
-			case "booty":
-				viewBooty();
-				break;
-			case "plunder":
-				plunderUser();
-				break;					
-			// default:
-			// 	msg.reply("Sorry, '" + cmd + "' is not a command. Please type '" + pre + "help' to learn more.");
-		}
+class Pirate {
+	constructor (msg) {
+		this.name = msg.author.username;
+		this.level = 1;
+		this.xp = 0;
+		this.gold = 500;
+		this.booty = {"Voodoo Doll":50};
 	}
-});
 
-function helpMenu(msg, cmd) {
+	getStats () {
+		var count = Object.keys(this.booty).length;
+		return `Here are your Stats:
+			Level: ${this.level}
+			XP: ${this.xp}
+			Gold: ${this.gold}
+			Booty: ${count} items`;
+	}
+
+	exploreChannel () {
+		return
+	}
+
+	pillageTreasure () {
+		return
+	}
+
+	getBooty () {
+		var reply = "Here's your stash of booty:\n"
+		for (const [key,value] of Object.entries(this.booty))
+			reply += `${key}. Value: ${value} gold.`
+		return reply
+	}
+
+	plunderUser () {
+		return
+	}
+
+	duelUser () {
+		return
+	}
+}
+
+function helpMenu (msg, cmd) {
 
 	switch (cmd[1]) {
 		case "config":
@@ -73,12 +77,13 @@ function helpMenu(msg, cmd) {
 			break;
 		case "plunder":
 			msg.reply(`\`${pre}plunder <pirate>\`: Attempts to steal loot and gold from another pirate.`);
+			break;
+		case "duel":
+			msg.reply(`\`${pre}plunder <pirate>\`: Battle another pirate.`);
 			break;	
 		default:
 			msg.reply(`here is the PirateBot Help Menu:
 				\`${pre}help\`: Brings up this menu.
-
-				\`${pre}config\`: Set PirateBot's configuration (Admin).
 
 				\`${pre}start\`: Starts your pirate adventure.
 				
@@ -90,9 +95,125 @@ function helpMenu(msg, cmd) {
 				
 				\`${pre}booty\`: Shows your loot stash.
 				
-				\`${pre}plunder <pirate>\`: Attempts to steal loot and gold from another pirate.`);
+				\`${pre}plunder <pirate>\`: Attempts to steal loot and gold from another pirate.
+				
+				\`${pre}duel <pirate>\`: Battle another pirate.
+				
+				\`${pre}config\`: Set PirateBot's configuration (Admin).
+				`);
 	}
-
 }
+
+function startAdventure (msg) {
+	//if pirate already exists
+	if (isPirate(msg)) {
+		//return message saying so
+		var pirate = sevenSeas[msg.author.username]
+		msg.reply(`You're already a pirate, Matey!\n` + pirate.getStats());
+	} else {
+		var pirate = new Pirate(msg);
+		sevenSeas[msg.author.username] = pirate;
+		msg.reply(`There's a new pirate on the seas! Make way for ${pirate.name}!\n` + pirate.getStats())	;	
+	}
+	return;
+}
+
+function getStats (msg) {
+	//if pirate doesn't exists
+	if (!isPirate(msg)) {
+		//return message saying so
+		msg.reply(`You're not yet a pirate! type ${pre}start to begin your adventure!`)
+	} else {
+		var pirate = sevenSeas[msg.author.username]
+		msg.reply(pirate.getStats())
+	}
+	return;
+}
+
+function exploreChannel (msg) {
+	msg.reply("This function has not been set up yet!")
+	return;
+}
+
+function pillageTreasure (msg) {
+	msg.reply("This function has not been set up yet!")
+	return;
+}
+
+function getBooty (msg) {
+	//if pirate doesn't exists
+	if (!isPirate(msg)) {
+		//return message saying so
+		msg.reply(`You're not yet a pirate! type ${pre}start to begin your adventure!`)
+	} else {
+		var pirate = sevenSeas[msg.author.username]
+		msg.reply(pirate.getBooty())
+	}
+	return;
+}
+
+function plunderUser (msg) {
+	msg.reply("This function has not been set up yet!")
+	return;
+}
+
+function duelUser (msg) {
+	msg.reply("This function has not been set up yet!")
+	return;
+}
+
+function setConfig (msg) {
+	return;
+}
+
+function isPirate (msg) {
+	return msg.author.username in sevenSeas;
+}
+
+
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if (msg.content.startsWith(pre)) {
+		var cmd = msg.content.replace(pre,"").split(" ");
+		console.log(`Command received from ${msg.author.tag}: ${cmd}`);
+		switch (cmd[0]) {
+			case "help":
+				helpMenu(msg, cmd);
+				break;
+			case "start":
+				startAdventure(msg);
+				break;
+			case "stats":
+				getStats(msg);
+				break;
+			case "explore":
+				exploreChannel(msg);
+				break;
+			case "pillage":
+				pillageTreasure(msg);
+				break;
+			case "booty":
+				getBooty(msg);
+				break;
+			case "plunder":
+				plunderUser(msg);
+				break;
+			case "duel":
+				duelUser(msg);
+				break;
+			case "config":
+				setConfig();
+				break;
+			// default:
+			// 	msg.reply("Sorry, '" + cmd + "' is not a command. Please type '" + pre + "help' to learn more.");
+		}
+	}
+});
+
+
 
 client.login(auth.token);
